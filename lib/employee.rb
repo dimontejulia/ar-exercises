615ea90f68d114725f95bbfb91b80190da07dfe5
dimontejulia <dimontejulia@gmail.com>
2021-01-04 21:29:03 -0500

    exercise 8

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,15 @@ class Employee < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, numericality: {only_integer: true, greater_than: 40, less_than: 200}
+
+  before_save :create_password
+
+  # It is considered good practice to declare callback methods as private. 
+  #If left public, they can be called from outside of the model and violate the principle of object encapsulation.
+  private
+  def create_password
+    range = [*'0'..'9',*'A'..'Z',*'a'..'z']
+    self.password = (0...8).map{ range.sample }.join
+  end
+
 end
