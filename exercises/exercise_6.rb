f2f91d244d7ae6bfdb0616a0cd7ee1c92e72c7d2
dimontejulia <dimontejulia@gmail.com>
2021-01-04 19:25:21 -0500

    exercise 6 - added employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 100)
+@store1.employees.create(first_name: "Ron", last_name: "Weasley", hourly_rate: 60)
+@store1.employees.create(first_name: "Cedric", last_name: "Diggory", hourly_rate: 65)
+@store2.employees.create(first_name: "Hermione", last_name: "Granger", hourly_rate: 90)
+@store4.employees.create(first_name: "Draco", last_name: "Malfoy", hourly_rate: 80)
+@store4.employees.create(first_name: "Ginny", last_name: "Weasley", hourly_rate: 50)
+@store5.employees.create(first_name: "Luna", last_name: "Lovegood", hourly_rate: 50)
+@store5.employees.create(first_name: "George", last_name: "Weasley", hourly_rate: 70)
+@store6.employees.create(first_name: "Fred", last_name: "Weasley", hourly_rate: 70)
