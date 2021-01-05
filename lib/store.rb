26e63cd2d9378700945d282fcb7cfe0751af4099
dimontejulia <dimontejulia@gmail.com>
2021-01-04 21:52:09 -0500

    exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -4,10 +4,20 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   validate :carry_apparel
 
+  before_destroy :check_destroy
+
   def carry_apparel 
     if (!womens_apparel.present? && !mens_apparel.present?)
       errors.add(:mens_apparel, "must select an apparel type") 
       errors.add(:womens_apparel, "must select an apparel type") 
     end
   end
+
+  private 
+  def check_destroy
+    if self.employees.size > 0
+      return false
+    end
+  end
+
 end
